c63f0c913dcc1981ad34b0e430b855cf4a38e099
rfreitasbatista <rfreitasbatista@gmail.com>
2020-07-24 18:45:38 -0300

    API - codeclimate arrumado

--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,22 @@
-
 export async function getCategories() {
-let url = "https://api.mercadolibre.com/sites/MLB/categories";
-let fetchURL = fetch(url).then((resp) => resp.json())
-return await fetchURL
+  let url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  let fetchURL = fetch(url).then((resp) => resp.json());
+  return await fetchURL;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-let url = ''
-if(categoryId && query) {
-url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`
+  let url = '';
+  if (categoryId && query) {
+    url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  }
+
+  if (categoryId && !query) {
+    url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  }
+
+  if (!categoryId && query) {
+    url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  }
+
+  return await fetch(url).then((response) => response.json());
 }
-
-if(categoryId && !query){
-url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`
-}
-
-if(!categoryId && query){
-url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`
-}
-
-return await fetch(url).then(response => response.json())
-}
-
-
