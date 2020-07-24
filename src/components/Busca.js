7d227f652772190fb2a91357bed93f0a801fcd93
rfreitasbatista <rfreitasbatista@gmail.com>
2020-07-24 20:37:10 -0300

    Pagina inicial

---
+++ src/components/Busca.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import * as apiFunction from '../services/api'
+
+class Busca extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    options : []
+    }
+  }
+  
+  componentDidMount() {
+    apiFunction.getCategories().then(response => {
+      this.setState({
+        options: response
+      })
+    })
+  }
+
+  filter (option, texto) {
+    apiFunction.getProductsFromCategoryAndQuery(option, texto)
+  }
+
+  render() {
+  const { options } = this.state
+  console.log(options)
+  return (
+    <div>
+      <form>
+        <h1>Digite algum termo de pesquisa ou escolha uma categoria.</h1>
+        <input type='text' />
+        <select>
+          {options.map(categoria => 
+            <option key={categoria.id}>{categoria.name}</option>
+          )}
+        </select>
+      </form>
+    </div>
+  )
+  }
+}
+
+export default Busca;
